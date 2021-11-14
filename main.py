bd971bf07128e76fa3dcb6d51113f741c1f11839
Philippe Vinchon <p.vinchon@gmail.com>
2021-11-14 11:29:36 +0000

    Standardize quotes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import urllib
 app = flask.Flask(__name__)
 limiter = flask_limiter.Limiter(app,
                                 key_func=flask_limiter.util.get_remote_address,
-                                default_limits=["200 per day", "50 per hour"])
+                                default_limits=['200 per day', '50 per hour'])
 
 method_requests_mapping = {
     'GET': requests.get,
@@ -24,7 +24,7 @@ method_requests_mapping = {
 
 @app.route('/')
 def index():
-    md = pathlib.Path("README.md").read_text()
+    md = pathlib.Path('README.md').read_text()
     html = markdown.markdown(md)
 
     return html
@@ -38,8 +38,8 @@ def proxy(url):
     request_function = method_requests_mapping[flask.request.method]
 
     request_headers = dict(flask.request.headers)
-    request_headers["Host"] = urllib.parse.urlparse(url).netloc
-    request_headers["User-Agent" = "cors.pub"
+    request_headers['Host'] = urllib.parse.urlparse(url).netloc
+    request_headers['User-Agent'] = 'cors.pub'
 
     request = request_function(url,
                                stream=True,
